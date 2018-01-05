3997e5faf08a4640a1101fd39571f49f073e6ac6
Fiona Lapham <fionadlapham@gmail.com>
2018-01-05 15:47:28 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -21,7 +21,7 @@ def retrieval
                  }
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 end
-
+shipping_manifest["oil paintings"]
 
 def adding
   shipping_manifest = {
@@ -41,4 +41,3 @@ def adding
 
 end
 
-shipping_manifest["oil paintings"]
